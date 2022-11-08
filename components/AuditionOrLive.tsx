d6668eacdc6c7b0a72c6b9073edc1e1c64d23b9c
yajamon <yajamon.tatsuki@gmail.com>
2022-11-09 00:46:50 +0900

    とりあえずオーディションおよびライブで得られるものを描画する

--- components/AuditionOrLive.tsx
+++ components/AuditionOrLive.tsx
@@ -5,10 +5,30 @@ interface Props {
 }
 
 export function AuditionOrLive(props: Props) {
-  const { name } = props.record;
+  const { name, kind, difficulty } = props.record;
+  const { fan, vo, da, vi, me, sp, love, bonus } = props.record;
   return (
-    <div>
-      {name}
+    <div class="flex flex-col">
+      <div class="flex flex-row">
+        <div class="flex-none p-3 w-44">
+          {kind}
+        </div>
+        <div class="flex-auto p-3">
+          {name}
+        </div>
+      </div>
+      <div class="flex flex-row">
+        <div class="flex-auto">ファン数: +{fan}</div>
+        <div class="flex-auto">歌唱力: +{vo}</div>
+        <div class="flex-auto">安定感: +{da}</div>
+        <div class="flex-auto">表現力: +{vi}</div>
+        <div class="flex-auto">集中力: +{me}</div>
+        <div class="flex-auto">団結力: +{sp}</div>
+      </div>
+      <div class="flex flex-row">
+        <div class="flex-auto">親愛度： +{love}</div>
+        <div class="flex-auto">ボーナス：{bonus}</div>
+      </div>
     </div>
   );
 }
