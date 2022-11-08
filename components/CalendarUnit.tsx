0675629112b11c1086b1f52a65bea92ee7264349
yajamon <yajamon.tatsuki@gmail.com>
2022-11-09 00:02:21 +0900

    とりあえずオーディションやライブの名前を出してみる

--- components/CalendarUnit.tsx
+++ components/CalendarUnit.tsx
@@ -1,5 +1,14 @@
 import { CalendarUnit as CalendarUnitData } from "./dataset.ts";
 
 export function CalendarUnit(props: { unit: CalendarUnitData }) {
-  return <div>シーズン{props.unit.season}、第{props.unit.week}週</div>;
+  //FIXME: 「お仕事」にはプロデュース内に別の文脈がある
+  const works = props.unit.works.map((record) => <div>{record.name}</div>);
+  return (
+    <div>
+      <div>
+        シーズン{props.unit.season}、第{props.unit.week}週
+      </div>
+      <div>{works}</div>
+    </div>
+  );
 }
