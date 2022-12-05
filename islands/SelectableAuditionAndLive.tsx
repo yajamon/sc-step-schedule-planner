8a940c28c99b4ea5f3749952b8e7a56402687f1d
yajamon <yajamon.tatsuki@gmail.com>
2022-12-05 16:31:25 +0900

    状態を持つ準備をする

--- islands/SelectableAuditionAndLive.tsx
+++ islands/SelectableAuditionAndLive.tsx
@@ -1,10 +1,22 @@
+import { useState } from "preact/hooks";
 import { Calendar } from "../components/dataset.ts";
 import { AuditionAndLive } from "../components/AuditionAndLive.tsx"
+
+interface ScheduleUnit {
+    // key
+    season: number;
+    week: number;
+    // value
+    auditionOrLiveId: string;
+}
+type Schedule = ScheduleUnit[];
 
 type Props = {
     calendar: Calendar,
 }
 export default function SelectableAuditionAndLive({calendar}: Props) {
+
+    const [schedule, setSchedule] = useState<Schedule>([]);
     return (
         <AuditionAndLive calendar={calendar} />
     );
